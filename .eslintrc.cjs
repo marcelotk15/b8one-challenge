aa169d58aa571af17c01331830b30c9cae6cfba1
Marcelo Oliveira <marcelotk_15@hotmail.com>
2023-05-03 13:49:50 -0300

    📦 NEW: lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,54 @@
-module.exports = {
-  env: { browser: true, es2020: true },
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  overrides: [
+    {
+      extends: ['plugin:@typescript-eslint/recommended-requiring-type-checking'],
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: path.join(__dirname, 'tsconfig.json'),
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: path.join(__dirname, 'tsconfig.json'),
+  },
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:import/typescript',
+    'prettier',
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    'prettier/prettier': 'error',
+    'import/first': 'error',
+    'import/no-duplicates': 'error',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+      },
+    ],
+    'import/newline-after-import': 'error',
+    'import/no-absolute-path': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/export': 'error',
+    'import/no-useless-path-segments': ['error', { commonjs: true }],
+    'import/no-self-import': 'error',
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
   },
 }
+
+module.exports = config
